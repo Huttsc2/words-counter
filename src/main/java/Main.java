43e5b97f8cd52db3a696c9c82287586d06c977cc
Mike <SicSid1@gmail.com>
2023-02-06 14:07:30 +0400

    qweqwe

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 class Main{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Area area = new Area(10, 20);
-        Dot dot = new Dot(5, 10);
+        Area area = new Area(15, 45);
+        Dot dot = new Dot(7, 22);
         Dot point = new Dot(3, 12);
         Draw draw = new Draw(area.getHeight(), area.getWidth(), dot.getStartX(), dot.getStartY(), point.getStartX(), point.getStartY());
         draw.draw(dot.getStartX(), dot.getStartY());
