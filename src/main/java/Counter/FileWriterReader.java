b262bf1f53d367f604544ba846b536dc3c425827
Mike <SicSid1@gmail.com>
2023-03-09 14:18:01 +0400

    file separate

--- src/main/java/Counter/FileWriterReader.java
+++ src/main/java/Counter/FileWriterReader.java
@@ -1,8 +1,39 @@
 package Counter;
 
-import java.io.File;
+import java.io.*;
+import java.util.Scanner;
 
 public class FileWriterReader {
-    File file;
+    String toRead;
+    String toWrite;
+    String fileText;
 
+    public FileWriterReader() throws FileNotFoundException {
+        Scanner sc = new Scanner(System.in);
+        this.toRead = sc.nextLine();
+        this.toWrite = "log.txt";
+        this.fileText = fileToString();
+    }
+    public String fileToString() throws FileNotFoundException {
+        Scanner sc = new Scanner(new File(toRead));
+        return sc.useDelimiter("\\A").next();
+    }
+    public void WriteToLog() throws FileNotFoundException {
+        WordCounter wordCounter = new WordCounter(fileToString());
+        try (FileWriter log = new FileWriter(toWrite)) {
+            log.write(wordCounter.displayToString());
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+    public void ReadLog() {
+        try (FileReader readLog = new FileReader(toWrite)) {
+            int c;
+            while ((c=readLog.read())!=-1) {
+                System.out.print((char)c);
+            }
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
 }
--- src/main/java/Counter/Main.java
+++ src/main/java/Counter/Main.java
@@ -5,23 +5,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        Scanner scanner = new Scanner(new File("FightClub.txt"));
-        String text = scanner.useDelimiter("\\A").next();
-        WordCounter wordCounter = new WordCounter(text);
-        try (FileWriter log = new FileWriter("log.txt")) {
-            log.write(wordCounter.displayToString());
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-        try (FileReader readLog = new FileReader("log.txt")) {
-            int c;
-            while ((c=readLog.read())!=-1) {
-                System.out.print((char)c);
-            }
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-        //todo sort words in AnyUnique by alphabet
+        FileWriterReader fileWriterReader = new FileWriterReader();
+        fileWriterReader.WriteToLog();
+        fileWriterReader.ReadLog();
         //todo choose type of sort
         //todo choose file
         //todo separate file to new method
